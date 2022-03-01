1e0531c947994f4d9dcb5fc6f00c6c8535589848
Andrew G Sorokin <angello@elm.dp.ua>
2022-03-01 13:06:37 +0200

    fix

--- ddos.sh
+++ ddos.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-docker stop ddos
-docker rm ddos
-docker rmi ddos
-docker build -t ddos . && docker run --rm -d --name ddos ddos && docker logs -f ddos
+/usr/bin/docker stop ddos
+/usr/bin/docker rm ddos
+/usr/bin/docker rmi ddos
+cd /root/stopWar && /usr/bin/docker build -t ddos . && /usr/bin/docker run --rm -d --name ddos ddos 
+#&& docker logs -f ddos
