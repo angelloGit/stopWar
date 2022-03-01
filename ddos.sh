a69402d389a349a7a07904f8f96bb43a241bba44
Andrew G Sorokin <angello@elm.dp.ua>
2022-03-02 00:11:37 +0200

    fix (((

--- ddos.sh
+++ ddos.sh
@@ -2,9 +2,10 @@
 
 cd /root/stopWar
 
+/usr/bin/docker stop ddos
 /usr/bin/docker pull angello2docker/nowarddos:latest
 /usr/bin/docker run -d --restart always --name ddos angello2docker/nowarddos:latest
 
-
+/usr/bin/docker stop ufo
 /usr/bin/docker pull freedomwarrior/ufo:v1
 /usr/bin/docker run -d --restart always --name ufo freedomwarrior/ufo:v1
