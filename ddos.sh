b61810bd3b4b999acf56bcc72536ec277f925a80
Andrew G Sorokin <angello@elm.dp.ua>
2022-03-01 20:32:52 +0200

    --rm romoved

--- ddos.sh
+++ ddos.sh
@@ -3,7 +3,7 @@
 /usr/bin/docker stop ddos
 /usr/bin/docker rm ddos
 /usr/bin/docker rmi ddos
-cd /root/stopWar && /usr/bin/docker build -t ddos . && /usr/bin/docker run --rm -d --name ddos ddos 
+cd /root/stopWar && /usr/bin/docker build -t ddos . && /usr/bin/docker run -d --name ddos ddos 
 #&& docker logs -f ddos
 
 /usr/bin/docker stop ufo
