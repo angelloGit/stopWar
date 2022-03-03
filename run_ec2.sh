2a0c573a701da29332cb3e7534f787cb73d1ee76
Andrew G Sorokin <angello@elm.dp.ua>
2022-03-03 18:04:29 +0200

    update

--- run_ec2.sh
+++ run_ec2.sh
@@ -4,16 +4,13 @@ set -e
 
 apt-get update
 apt-get upgrade -y
-apt-get install -y git
 
 cd /root && /usr/bin/git clone https://github.com/angelloGit/stopWar
 cd /root/stopWar
 
 ./install.sh
-
 ./ddos.sh
 
 crontab - <<__EOFF
 */7 * * * * cd /root/stopWar/ && /usr/bin/git pull
-31 * * * * /root/stopWar/ddos.sh
 __EOFF
