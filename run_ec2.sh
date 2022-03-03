69bdebbeb59aae96ce13beb962ec2f2bfdb839e4
Andrew G Sorokin <angello@elm.dp.ua>
2022-03-03 18:17:12 +0200

    update run_ec2

--- run_ec2.sh
+++ run_ec2.sh
@@ -11,6 +11,14 @@ cd /root/stopWar
 ./install.sh
 ./ddos.sh
 
+cat > .git/hooks/post-merge <<__EOFF
+#!/bin/sh
+
+./ddos.sh > last.log
+__EOFF
+
+chmod a+x .git/hooks/post-merge
+
 crontab - <<__EOFF
 */7 * * * * cd /root/stopWar/ && /usr/bin/git pull
 __EOFF
